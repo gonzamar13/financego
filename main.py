bc8522155b9718a6123650fe03f560cd7877173e
gonzamar13 <marcosgonzs13@gmail.com>
2026-04-02 18:12:41 -0300

    Se prepara para levantar en dokploy

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 from sqlalchemy.orm import Session
+
 from app.routers.auth import router as auth_router
 from app.routers.accounts import router as accounts_router
 from app.routers.category import router as categories_router
+from app.routers.transaction import router as transactions_router
+
 from app.db.base import Base
 from app.db.session import engine, get_db
+
 from app.models.user import User
 from app.models.account import Account
 from app.models.category import Category
-from app.routers.transaction import router as transactions_router
+from app.models.transaction import Transaction
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="FinanceGO")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://app.financego.cloud",
+        "https://dash.financego.cloud",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 app.include_router(accounts_router)
 app.include_router(categories_router)
@@ -27,6 +45,5 @@ def health_check():
 def db_test(db: Session = Depends(get_db)):
     db.execute(text("SELECT 1"))
     return {"db": "ok"}
-
 
 
